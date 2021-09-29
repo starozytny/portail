2d16178cde32e728dd148b4b1493a5cad8c4fcf9
Shanbora <chanbora@logilink.fr>
2021-09-29 12:17:08 +0200

    split code submit form

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -18,6 +18,94 @@ class UserController
         $this->session = $session;
     }
 
+    private function submitForm($data, $url)
+    {
+        $error0 = ['name' => 'firstname', 'message' => 'Cet champ doit être renseigné.'];
+        $error1 = ['name' => 'lastname', 'message' => 'Cet champ doit être renseigné.'];
+        $error2 = ['name' => 'email', 'message' => 'Cet champ doit être renseigné.'];
+        $error3 = ['name' => 'userTag', 'message' => 'Cet champ doit être renseigné.'];
+        $errors = [];
+
+        $firstname = $data->firstname;
+        $lastname = $data->lastname;
+        $password = isset($data->password) ?: "";
+        $email = $data->email;
+        $userTag = $data->userTag;
+        if($firstname != "" && $lastname != "" && $email != ""){
+
+            $res = $this->apiService->callApi($url, 'POST', false, [
+                'first_name' => $firstname,
+                'last_name' => $lastname,
+                'password' => $password,
+                'email' => $email,
+                'user_tag' => $userTag
+            ]);
+            if($res == false){
+                return "[UU001] Une erreur est survenu. Veuillez contacter le support.";
+            }
+
+            if($data->main){
+                $user = [
+                    $this->session->get('user')[0],
+                    $this->session->get('user')[1],
+                    $firstname,
+                    $lastname,
+                    $this->session->get('user')[4],
+                    $email,
+                    $userTag,
+                    $this->session->get('user')[7]
+                ];
+
+                $this->session->destroy();
+                $this->session->start();
+                $this->session->regenerateId();
+
+                $this->session->set('user', $user);
+            }
+
+            return 1;
+        }else{
+            if($firstname == ""){
+                array_push($errors, $error0);
+            }
+            if($lastname == ""){
+                array_push($errors, $error1);
+            }
+            if($email == ""){
+                array_push($errors, $error2);
+            }
+            if($userTag == ""){
+                array_push($errors, $error3);
+            }
+        }
+
+        return $errors;
+    }
+
+    /**
+     * Route pour ajouter un utilisateur
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param array $args
+     * @return ResponseInterface
+     */
+    public function create(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $response->withHeader('Content-Type', 'application/json');
+
+        $data = json_decode($request->getBody());
+        $res = $this->submitForm($data, 'add_user/');
+
+        if($res != 1){
+            $response->getBody()->write($res);
+            return $response->withStatus(400);
+        }
+
+        $response->getBody()->write("Utilisateur ajouté.");
+        return $response->withStatus(200);
+    }
+
     /**
      * Route pour modifier un utilisateur
      *
@@ -29,62 +117,16 @@ class UserController
     public function update(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
         $response->withHeader('Content-Type', 'application/json');
-        $error0 = ['name' => 'firstname', 'message' => 'Cet champ doit être renseigné.'];
-        $error1 = ['name' => 'lastname', 'message' => 'Cet champ doit être renseigné.'];
-        $error2 = ['name' => 'email', 'message' => 'Cet champ doit être renseigné.'];
-        $errors = [];
 
         $data = json_decode($request->getBody());
-        $firstname = $data->firstname;
-        $lastname = $data->lastname;
-        $email = $data->email;
-        if($firstname != "" && $lastname != "" && $email != ""){
+        $res = $this->submitForm($data, 'edit_user/' . $args['id']);
 
-            $res = $this->apiService->callApi('edit_user/' . $args['id'], 'POST', false, [
-                'first_name' => $firstname,
-                'last_name' => $lastname,
-                'email' => $email,
-                'user_tag' => $data->userTag
-            ]);
-            if($res == false){
-                $response->getBody()->write("[UU001] Une erreur est survenu. Veuillez contacter le support.");
-                return $response->withStatus(400);
-            }
-
-            if($data->main){
-                $user = [
-                    $this->session->get('user')[0],
-                    $this->session->get('user')[1],
-                    $firstname,
-                    $lastname,
-                    $this->session->get('user')[4],
-                    $email,
-                    $this->session->get('user')[6],
-                    $this->session->get('user')[7]
-                ];
-
-                $this->session->destroy();
-                $this->session->start();
-                $this->session->regenerateId();
-
-                $this->session->set('user', $user);
-            }
-
-            $response->getBody()->write($firstname . " " . $lastname . " mis à jour.");
-            return $response->withStatus(200);
-        }else{
-            if($firstname == ""){
-                array_push($errors, $error0);
-            }
-            if($lastname == ""){
-                array_push($errors, $error1);
-            }
-            if($email == ""){
-                array_push($errors, $error2);
-            }
+        if($res != 1){
+            $response->getBody()->write($res);
+            return $response->withStatus(400);
         }
 
-        $response->getBody()->write(json_encode($errors));
-        return $response->withStatus(400);
+        $response->getBody()->write("Données mises à jour.");
+        return $response->withStatus(200);
     }
 }
