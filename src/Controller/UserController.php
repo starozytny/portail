ad5006f21dd9712494cd753bd93b0e2e5810482d
Shanbora <chanbora@logilink.fr>
2021-09-29 15:52:01 +0200

    add comment

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -24,90 +24,6 @@ class UserController
         $this->sanitizeData = $sanitizeData;
     }
 
-    private function submitForm($data, $url)
-    {
-        $formFrom = $data->formFrom;
-        $username = $this->session->get('user')[0];
-        $firstname = $this->sanitizeData->clean($data->firstname);
-        $lastname = $this->sanitizeData->clean($data->lastname);
-        $password = $data->password ?? "";
-        $email = $this->sanitizeData->clean($data->email);
-        $userTag = $data->userTag;
-
-        $paramsToValidate = [
-            ['type' => 'text', 'name' => $formFrom . '-firstname',    'value' => $firstname],
-            ['type' => 'text', 'name' => $formFrom . '-lastname',     'value' => $lastname],
-            ['type' => 'text', 'name' => $formFrom . '-email',        'value' => $email]
-        ];
-
-        if($formFrom == "create"){
-            array_push($paramsToValidate, ['type' => 'text', 'name' => $formFrom . '-password', 'value' => $password]);
-        }
-
-        $errors = $this->validateur->validate($paramsToValidate);
-
-        if(count($errors) > 0){
-            return json_encode($errors);
-        }
-
-        if($formFrom == "create"){
-            //create userTag, username
-            $i = 0;
-            $uid = round(microtime(true));
-            $loginId = substr($uid, 3, -2);
-
-            $username = $this->session->get('user')[8] . $loginId;
-            $userTag = mb_strtoupper(substr($firstname, 0, 3));
-
-            $users = $this->apiService->callApi('users');
-            foreach($users as $user){
-                if($user->username == $username){
-                    $loginId++;
-                    $username = $this->session->get('user')[8] . $loginId;
-                }
-                if($user->user_tag == $userTag){
-                    $i++;
-                    $userTag = $userTag . $i;
-                }
-            }
-        }
-
-        $res = $this->apiService->callApi($url, 'POST', false, [
-            'username' => $username,
-            'first_name' => $firstname,
-            'last_name' => $lastname,
-            'password' => $password,
-            'email' => $email,
-            'user_tag' => $userTag
-        ]);
-        if($res == false){
-            return "[UU001] Une erreur est survenu. Veuillez contacter le support.";
-        }
-
-        if($formFrom == "main"){
-            $user = [
-                $this->session->get('user')[0],
-                $this->session->get('user')[1],
-                $firstname,
-                $lastname,
-                $this->session->get('user')[4],
-                $email,
-                $userTag,
-                $this->session->get('user')[7],
-                $this->session->get('user')[8],
-                $this->session->get('user')[9],
-            ];
-
-            $this->session->destroy();
-            $this->session->start();
-            $this->session->regenerateId();
-
-            $this->session->set('user', $user);
-        }
-
-        return 1;
-    }
-
     /**
      * Route pour ajouter un utilisateur
      *
@@ -154,6 +70,97 @@ class UserController
 
         $response->getBody()->write("Données mises à jour.");
         return $response->withStatus(200);
+    }
+
+    /**
+     * Method pour envoyer le formulaire de création et édition d'un utilisateur
+     *
+     * @param $data
+     * @param $url
+     * @return false|int|string
+     */
+    private function submitForm($data, $url)
+    {
+        $formFrom = $data->formFrom;
+        $username = $this->session->get('user')[0];
+        $firstname = $this->sanitizeData->clean($data->firstname);
+        $lastname = $this->sanitizeData->clean($data->lastname);
+        $password = $data->password ?? "";
+        $email = $this->sanitizeData->clean($data->email);
+        $userTag = $data->userTag;
+
+        // validation des données
+        $paramsToValidate = [
+            ['type' => 'text', 'name' => $formFrom . '-firstname',    'value' => $firstname],
+            ['type' => 'text', 'name' => $formFrom . '-lastname',     'value' => $lastname],
+            ['type' => 'text', 'name' => $formFrom . '-email',        'value' => $email]
+        ];
+        if($formFrom == "create"){
+            array_push($paramsToValidate, ['type' => 'text', 'name' => $formFrom . '-password', 'value' => $password]);
+        }
+        $errors = $this->validateur->validate($paramsToValidate);
+        if(count($errors) > 0){
+            return json_encode($errors);
+        }
+
+        if($formFrom == "create"){
+            //create userTag, username
+            $i = 0;
+            $uid = round(microtime(true));
+            $loginId = substr($uid, 3, -2);
+
+            $username = $this->session->get('user')[8] . $loginId;
+            $userTag = mb_strtoupper(substr($firstname, 0, 3));
+
+            $users = $this->apiService->callApi('users');
+            foreach($users as $user){
+                if($user->username == $username){
+                    $loginId++;
+                    $username = $this->session->get('user')[8] . $loginId;
+                }
+                if($user->user_tag == $userTag){
+                    $i++;
+                    $userTag = $userTag . $i;
+                }
+            }
+        }
+
+        //Appel API
+        $res = $this->apiService->callApi($url, 'POST', false, [
+            'username' => $username,
+            'first_name' => $firstname,
+            'last_name' => $lastname,
+            'password' => $password,
+            'email' => $email,
+            'user_tag' => $userTag
+        ]);
+        if($res == false){
+            return "[UU001] Une erreur est survenu. Veuillez contacter le support.";
+        }
+
+        //regeneration des variables en sessions
+        if($formFrom == "main"){
+            $user = [
+                $this->session->get('user')[0],
+                $this->session->get('user')[1],
+                $firstname,
+                $lastname,
+                $this->session->get('user')[4],
+                $email,
+                $userTag,
+                $this->session->get('user')[7],
+                $this->session->get('user')[8],
+                $this->session->get('user')[9],
+            ];
+
+            $this->session->destroy();
+            $this->session->start();
+            $this->session->regenerateId();
+
+            $this->session->set('user', $user);
+        }
+
+        return 1;
     }
 
     /**
