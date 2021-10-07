c1e933029c9e9ee0e8f115bfa4e6025ea5cb9a6d
Shanbora <chanbora@logilink.fr>
2021-10-07 12:02:23 +0200

    init properties react

--- assets/js/pages/property.js
+++ assets/js/pages/property.js
@@ -1,1 +1,10 @@
 import '../../css/pages/property.scss';
+
+import React from "react";
+import { render } from "react-dom";
+import { Properties } from "./components/Property/Properties";
+
+let el = document.getElementById('properties');
+if(el){
+    render(<Properties {...el.dataset} />, el);
+}
