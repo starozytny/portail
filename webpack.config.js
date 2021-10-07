ba709804a92870bee8e007e8e2f1a82bd4422356
Shanbora <chanbora@logilink.fr>
2021-10-07 11:52:38 +0200

    add react

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,20 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.?(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
         ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
     },
     plugins: [
         new CleanWebpackPlugin(),
