8eaf945f5b32fab63b739fb956fa0ad49380dd06
Shanbora <chanbora@logilink.fr>
2021-10-12 11:02:10 +0200

    menu bibli

--- assets/js/pages/components/Bibli/Bibli.jsx
+++ assets/js/pages/components/Bibli/Bibli.jsx
@@ -1,18 +1,63 @@
 import React, { Component } from "react";
 
+import { Button } from "@dashboardComponents/Tools/Button";
+import { Alert }  from "@dashboardComponents/Tools/Alert";
+
+import { Rooms }  from "./Rooms";
+
 export class Bibli extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            context: null,
             dataImmuable: JSON.parse(props.donnees)
         }
+
+        this.handleChangeContext = this.handleChangeContext.bind(this);
     }
+
+    handleChangeContext = (context) => { this.setState({ context }) }
+
     render () {
-        const { dataImmuable } = this.state;
+        const { context, dataImmuable } = this.state;
 
         console.log(dataImmuable)
 
-        return <div>pkl</div>
+        let content;
+        switch (context){
+            case "rooms":
+                content = <Rooms />
+                break;
+            default:
+                content = <Alert>Veuillez cliquer sur un bouton (ci-dessus) pour afficher une liste d'éléments.</Alert>
+                break;
+        }
+
+        let menu = [
+            {value: "rooms",    label: "Pièces"},
+            {value: "keys",     label: "Clés"},
+            {value: "counters", label: "Compteurs"},
+            {value: "elements", label: "Elements"},
+            {value: "natures",  label: "Natures"},
+            {value: "aspects",  label: "Aspects"},
+        ]
+
+        return <>
+            <div className="toolbar">
+                {menu.map(item => {
+                    let active = item.value === context;
+                    return <div className="item">
+                        <Button outline={!active} type={active ? "color0" : "default"} onClick={() => this.handleChangeContext(item.value)}>{item.label}</Button>
+                    </div>
+                })}
+
+            </div>
+            <div className="items-table">
+                <div className="items items-default">
+                    {content}
+                </div>
+            </div>
+        </>
     }
 }
