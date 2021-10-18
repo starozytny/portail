2858746ed51eb0682de6b055ea1a469652a26b4b
Shanbora <chanbora@logilink.fr>
2021-10-18 17:18:30 +0200

    can remove room with swal alert

--- assets/js/pages/components/Modele/RoomItem.jsx
+++ assets/js/pages/components/Modele/RoomItem.jsx
@@ -1,5 +1,8 @@
 import React, {Component} from "react";
 
+import Swal         from "sweetalert2";
+import SwalOptions  from "@dashboardComponents/functions/swalOptions";
+
 import Sanitaze from "@dashboardComponents/functions/sanitaze";
 
 import { ButtonIcon } from "@dashboardComponents/Tools/Button";
@@ -13,30 +16,42 @@ export class RoomItem extends Component {
         }
 
         this.handleShow = this.handleShow.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
     }
 
     handleShow = () => { this.setState({ showDetails: !this.state.showDetails }) }
 
+    handleRemove = (uid) => {
+        const self = this;
+        Swal.fire(SwalOptions.options("Etes-vous sûr de vouloir supprimer cette pièce ?", ""))
+            .then((result) => {
+                if (result.isConfirmed) {
+                    self.props.onRemoveRoom(true, uid);
+                }
+            })
+        ;
+    }
+
     render () {
-        const { elem, library, onRemoveRoom } = this.props;
+        const { elem, library } = this.props;
         const { showDetails } = this.state;
 
-        return <div className={"item item-room" + (showDetails ? " active" : "")} onClick={this.handleShow}>
+        return <div className={"item item-room" + (showDetails ? " active" : "")}>
             <div className="item-content">
                 <div className="item-body">
                     <div className="infos infos-col-3">
-                        <div className="col-1">
+                        <div className="col-1" onClick={this.handleShow}>
                             <div className="name">
                                 {getStringData(library.rooms, elem.id)}
                             </div>
                         </div>
-                        <div className="col-2">
+                        <div className="col-2" onClick={this.handleShow}>
                             <Elements elements={JSON.parse(elem.elements)} library={library}/>
                         </div>
                         <div className="col-3 actions">
                             <ButtonIcon icon={showDetails ? "hide" : "show"} onClick={this.handleShow}>Détails</ButtonIcon>
                             <ButtonIcon icon="compose">Modifier</ButtonIcon>
-                            <ButtonIcon icon="delete" onClick={() => onRemoveRoom(elem.uid)}>Supprimer</ButtonIcon>
+                            <ButtonIcon icon="delete" onClick={() => this.handleRemove(elem.uid)}>Supprimer</ButtonIcon>
                         </div>
                     </div>
                 </div>
